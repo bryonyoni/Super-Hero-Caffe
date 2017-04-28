6ec274721997fe8a30f850b18b3a17bfe782506b
bryonyoni <bryonyoni@gmail.com>
2017-04-28 14:29:03 +0300

    created a Hero class
    Created a hero class containing all of the instances of the Heroes being
    created and a hero constructor.

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -3,4 +3,21 @@ import static org.junit.Assert.*;
 
 public class HeroTest {
 
+  // @After
+  //   public void tearDown() {
+  //   Task.clear();
+  // }
+
+  @Test
+  public void Hero_instantiatesCorrectly_true(){
+    Hero myHero = new Hero("spiderman");
+    assertEquals(true,myHero instanceof Hero);
+  }
+
+  @Test
+  public void Hero_instantiatesWithDescription_String(){
+    Hero myHero = new Hero("spiderman");
+    assertEquals("spiderman",myHero.getName());
+  }
+
 }
